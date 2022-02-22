ff37122d03e61552dffe8ac72dd40cf35a1f7e50
Knyazev Yaroslav <knyazev.yaroslav2020@gmail.com>
2022-02-22 13:13:14 +0300

    проверял себя

---
+++ lesson_1/home_work/index.js
@@ -0,0 +1,40 @@
+// let zxc = "123zxcghoul123"
+
+
+// console.log(typeof Number(zxc) === "number")
+// console.log(typeof Number(zxc))
+// console.log(typeof Number("123zxc") === "number")
+
+// console.log(typeof Number(undefined))
+
+// console.log(typeof 5 == "string")
+// let zxc = {};
+// let cxz = {};
+// console.log(JSON.stringify({a: 5}) === JSON.stringify({a: 5}))
+
+// 5 > 4
+// "ананас" > "яблоко"
+// "2" > "12"
+// undefined == null
+// undefined === null
+// null == "\n0\n"
+// null === +"\n0\n"
+// "" + 1 + 0
+// "" - 1 + 0
+// true + false
+// 6 / "3"
+
+
+// "2" * "3"
+// 4 + 5 + "px"
+// "$" + 4 + 5
+// "4" - 2
+// "4px" - 2
+// 7 / 0
+// "  -9  " + 5
+// "  -9  " - 5
+// null + 1
+// undefined + 1
+// " \t \n" - 2
+
+console.log(" \t \n" - 2)
