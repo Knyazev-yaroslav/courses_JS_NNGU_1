2fbadd147e5c218dd40feb23974bccc99cbc4ffd
Knyazev Yaroslav <knyazev.yaroslav2020@gmail.com>
2022-03-04 23:03:55 +0300

    home_work

---
+++ lesson_3/home_work/index.js
@@ -0,0 +1,59 @@
+console.log("hello World!");
+let myString = "Строка";
+
+let myNumber = 5;
+
+let myBoolean = true;
+
+let myNull = null;
+
+let myUndifined = undefined;
+
+console.log(myBigInt);
+
+console.log(myString);
+
+console.log(myBoolean);
+
+console.log(myNull);
+
+console.log(myNumber);
+
+console.log(myUndifined);
+
+console.log(typeof myString);
+
+console.log(typeof myBoolean);
+
+const myObject = {
+    name: "Oleg",
+    age: 28,
+};
+
+console.log(typeof myObject);
+
+// myObject = "user123"
+
+myObject.lastname = "Sokolov";
+
+console.log(myObject);
+
+let myObject = {
+    name: "Oleg",
+    age: 28,
+};
+
+myObject = "Mogol";
+
+console.log(myObject);
+
+var myObject = {
+    name: "Oleg",
+    age: 28,
+};
+
+myObject = "Mogol";
+
+console.log(myObject);
+
+
---
+++ lesson_3/index.js
@@ -0,0 +1,104 @@
+// //Примитивные типы данных:
+
+// // Number
+
+// console.log(1 / 0);
+
+// // string
+
+// let myString = "строка";
+
+// console.log(typeof myString);
+
+// let tempalString = `шаблонная строка ${myString}`;
+
+// console.log(tempalString);
+
+// // bolean
+
+// let isAdmin = true
+// let isUser = false
+
+// // null
+
+// let emptyVariable = null
+
+// // undifiend
+
+// let userName 
+// console.log(userName)
+
+// Непримитивные типы данных 
+// object
+
+
+// 
+// let user = {
+//     name:"andrey",
+//     lastName: "kudimov",
+//     age: "18",
+//     sitizenship: "Russian",
+
+// }
+
+// Оператор typeof
+
+// typeof null - всегда object!
+
+// console.log(typeof null);
+
+// Преобразование типов
+// Строковое преобразование
+
+// let myVar = true;
+
+// let myVarToString = String(myVar);
+// console.log(myVarToString);
+
+// численное преобразование
+
+// let myVar = "123";
+
+// const myVarToNumber = Number(myVar);
+
+// console.log(typeof myVarToNumber);
+
+// Логическуое преобразование
+
+// let myVar = 1;
+
+// console.log(!!myVar);
+
+// Математические операторы
+// Бинарный плюс
+
+// let myNumber = 1;
+// let newNumber = 2;
+// let myString = "Строка";
+
+// console.log (myNumber + myString);
+
+// Сравнение строк
+
+// let string1 = "строка";
+// let string2 = "столица";
+
+// console.log(string1 == string2);
+
+// unicode
+
+// let userTypes = {
+//     admin: "admin",
+//     user: "user",
+//     teacher: "teacher",
+//     contentManage: "contentManager"
+// }
+
+// let serverDataUserType = "user"
+
+// if (serverDataUserType === userTypes.user) {
+//     console.log("true")
+// } else {
+//     console.log("false")
+// }
+
