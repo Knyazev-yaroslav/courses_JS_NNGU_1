05a94d5f6497551547feee02dacd5e57cff58317
Knyazev Yaroslav <knyazev.yaroslav2020@gmail.com>
2022-03-07 15:36:20 +0300

    cw

---
+++ lesson_4/class_work/index.js
@@ -0,0 +1,61 @@
+// let myVar1 = 5;
+
+// let myVar2 = 10;
+
+// if (myVar1 > myVar2) {
+//     console.log(`Верно, ${myVar1} > ${myVar2}`)
+// } else if (myVar1 < myVar2) {
+//     console.log(`${myVar1} меньше ${myVar2}`)
+// } else {
+//     "end"
+// };
+
+// let myVar = myVar1 > myVar2 ? console.log("true") : console.log("false")
+
+// let userRole = 'admin';
+
+// switch(userRole) {
+//     case 'student' :
+//         console.log("Текущий пользователь student");
+//         break
+//     case 'admin' :
+//         console.log("Текущий пользователь admin");
+//         break;
+//     case "normal_user":
+//         console.log("Текущий пользователь normal_user")
+//         break
+//     default:
+//         console.log('нет такого типа пользователя');
+//         break;
+// }
+
+// Логичекие операторы:
+// && - логическое И - возвращает первое false
+// || - логическое ИЛИ - возвращает первое true
+// ! - логическое Не
+
+
+// console.log(0 || 0 || "");
+// console.log(1 && 2 && "строка" && 0 && 1)
+
+// console.log(1 && 2) || (4 && 0)
+
+// let a = 0;
+// let b = 1;
+
+// Логическое НЕ:
+
+let myVar = 5
+
+if(myVar){
+    console.log("True")
+}
+
+console.log(null || 0 || "" || undefined);
+
+console.log(null  ||  0  ||  ""  ||  undefined)
+console.log("яблоко"  &&  true  &&  null  &&  1 )
+console.log (0  ||  true  &&  "false"  ||  null)
+console.log(0  &&  true  ||  "false"  &&  null)
+console.log (!0 && !!1)
+console.log (!(null || !"апельсин" && true)  )
