9ed9b8485bd9289e1c1ba2b336cb232b1a786267
Knyazev Yaroslav <knyazev.yaroslav2020@gmail.com>
2022-03-19 09:11:27 +0300

    hw

--- lesson_5/home_work/index.js
+++ lesson_5/home_work/index.js
@@ -71,33 +71,36 @@
 
 // 4
 
-// let myFunc = (dayNumber) => {
-//     if(dayNumber >= 1 && dayNumber <= 7){
-//         switch (dayNumber){
-//             case 1:
-//                 console.log('Понедельник')
-//                 break
-//             case 2:
-//                 console.log('Вторник')
-//                 break
-//             case 3:
-//                 console.log('Среда')
-//                 break
-//             case 4:
-//                 console.log('Четверг')
-//                 break
-//             case 5:
-//                 console.log('Пятница')
-//                 break
-//             case 6:
-//                 console.log('Суббота')
-//                 break
-//             case 7:
-//                 console.log('Воскресенье')
-//                 break
-//         }
-//     } else console.log('Введите число от 1 до 7')
-// }
+let myFunc = (dayNumber) => {
+    if(dayNumber >= 1 && dayNumber <= 7){
+        switch (dayNumber){
+            case 1:
+                console.log('Понедельник')
+                break
+            case 2:
+                console.log('Вторник')
+                break
+            case 3:
+                console.log('Среда')
+                break
+            case 4:
+                console.log('Четверг')
+                break
+            case 5:
+                console.log('Пятница')
+                break
+            case 6:
+                console.log('Суббота')
+                break
+            case 7:
+                console.log('Воскресенье')
+                break
+            default:
+                console.log('Введите число от 1 до 7')
+                break
+        }
+    } 
+}
 
 // myFunc(3)
 
